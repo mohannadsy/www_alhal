070782df6bcc954614dde982671ada1188405bd0
rgd <raghad.nanou@gmail.com>
2021-12-19 12:45:45 +0200

    category

--- category_card.php
+++ category_card.php
@@ -11,26 +11,47 @@ include('include/nav.php');
     </head>
     <body>
 
-        <form action="" method="post"> 
-            
-            <label for="name">اسم الصنف</label>
-            <input type="text" name="name" id="">
-            <br><br>
-            <label for="code">رمز الصنف</label>
-            <input type="text" name="code" id="">
-            <br><br>
-            <label for="note"> ملاحظات</label>
-            <input type="text" name="note" id="">
-            <br><br>
-            <div>
-                <button type="submit" name="add">إضافة</button>
-                <button type="submit" name="update">تعديل</button>
-                <button type="submit" name="delete">حذف</button>
-                <button type="button" name="close">إغلاق</button>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group row">
+                    <label for="name" class="col-md-4 col-form-label text-md-right">اسم الصنف</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" name="name" required autofocus>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="code" class="col-md-4 col-form-label text-md-right">رمز الصنف</label>
+                    <div class="col-md-6">
+                        <input type="number" id="" class="form-control" name="code" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="note" class="col-md-4 col-form-label text-md-right"> ملاحظات</label>
+                    <div class="col-md-6">
+                        <input type="text" id="" class="form-control" name="note" required>
+                    </div>
+                </div>
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary" name="add">
+                        إضافة
+                    </button>
+                    <button type="submit" class="btn btn-primary" name="update">
+                        تعديل
+                    </button>
+                    <button type="submit" class="btn btn-primary" name="delete">
+                        حذف
+                    </button>
+                    <button type="button" class="btn btn-primary" name="close">
+                        إغلاق
+                    </button>
+                        
+                    </div>
+            <div class="col-6">
+
             </div>
-
-
-        </form>
+    </div>
         
     </body>
 </html>
