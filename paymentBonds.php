5453c67e5574f762147746c00c2add159714fe1a
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2021-12-20 13:46:01 -0800

    payment

---
+++ paymentBonds.php
@@ -0,0 +1,24 @@
+<?php
+include('include/nav.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form action="" method="post">
+    <div class="container">
+        
+    </div>
+  </form>  
+</body>
+</html>
+
+<?php
+include('include/footer.php');
+?>
--- test_sara.php
+++ test_sara.php
@@ -7,6 +7,8 @@ include('include/nav.php');
 <a href="open_file.php">Open file</a>
 <br>
 <a href="test_com_bill.php">Test print</a>
+<br>
+<a href="paymentBonds.php">Payment Bonds</a>
 <?php
 include('include/footer.php');
 ?>
