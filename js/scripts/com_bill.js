fe8a6216ced2d9eca9ae87cf7337cf7641f9f823
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2021-12-20 12:27:12 -0800

    default Rows

--- js/scripts/com_bill.js
+++ js/scripts/com_bill.js
@@ -1,17 +1,3 @@
-/*
-function addColumn() {
-    [...document.querySelectorAll('table tr')].forEach((row, i) => {
-        const input = document.createElement("th")
-        input.setAttribute('type', 'text')
-        const cell = document.createElement(i ? "td" : "th")
-        cell.appendChild(input)
-        row.appendChild(cell)
-    });
- }
- 
- document.querySelector('button').onclick = addColumn
-
-*/
 function createCell(cell, text, style) {
     var div = document.createElement('div'), // create DIV element
         txt = document.createTextNode(text); // create text node
@@ -38,7 +24,7 @@ function appendRow() {
         row = tbl.insertRow(tbl.rows.length), // append table row
         i;
     // insert table cells to the new row
-    if (tbl.rows.length < 11)
+    if (tbl.rows.length < 20)
         for (i = 0; i < tbl.rows[0].cells.length; i++) {
             createCell(row.insertCell(i), i, 'row');
         }
@@ -46,34 +32,16 @@ function appendRow() {
 
 document.querySelector('#add_col').onclick = appendColumn
 document.querySelector('#add_row').onclick = appendRow
-    /*
-    function addColumn(tblSample)
-    {
-    	var tblHeadObj = document.getElementById(tblSample).tHead;
-    	for (var h=0; h<tblHeadObj.rows.length; h++) {
-    		var newTH = document.createElement('th');
-    		tblHeadObj.rows[h].appendChild(newTH);
-    		newTH.innerHTML = h +  (tblHeadObj.rows[h].cells.length - 1)
-    	}
 
-    	var tblBodyObj = document.getElementById(tblSample).tBodies[0];
-    	for (var i=0; i<tblBodyObj.rows.length; i++) {
-    		var newCell = tblBodyObj.rows[i].insertCell(-1);
-    		newCell.innerHTML =   i + (tblBodyObj.rows[i].cells.length - 1)
-    	}
+
+
+
+function deafultRows() {
+    var i = 0;
+    for (i = 0; i < 10; i++) {
+        appendRow();
     }
+}
 
-    function addColumn(){
-        var rows=document.getElementById('tbl').getElementsByTagName('tr'), i=0, r, c, clone;
-            while(r=rows[i++]){
-            c=r.getElementsByTagName('td');
-            clone=cloneEl(c[c.length-1]);
-            c[0].parentNode.appendChild(clone);
-            }
-        }
-    function cloneEl(el){
-        var clo=el.cloneNode(true);
-        return clo;
-        }
-    document.querySelector('button').onclick = addColumn
-    */
+
+deafultRows();
