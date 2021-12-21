55b50a8955a0faf969d36fcda531b9d04b89af58
rgd <raghad.nanou@gmail.com>
2021-12-21 15:14:23 +0200

    paymentandcatch

--- catchBonds.php
+++ catchBonds.php
@@ -13,54 +13,74 @@ include('include/nav.php');
 <body>
   <form action="" method="post">
     <div class="container">
-      <div class="row "> 
-         <div class="col-6 " >
-            <label> الحساب </label>
-             <input type="text" name ="current" placeholder="الصندوق الأساسي" disabled>
-         </div>
-        <div class="col-6">
-          <label name=" "> رقم الإيصال </label>
-          <input type=number name =" " value= '' >
-        </div>
-        <div class="col-6">
-          <label name=" "> العملة </label>
-          <input type="text" name =" " value= '' >
-        </div>
-        <div class="col-6">
-           <label name=""> ملاحظات </label>
-              <textarea rows="2" type="text" name="note"> </textarea>
-        </div>
-        <div class="col-6">
-          <label name=" "> التاريخ</label>
-          <input type="date" name =" " value='' >
-        </div>
-     </div>
-<table  class="table table-bordered table-hover">
-<thead>
-  <tr>
-    <th> الحساب</th>
-    <th> دائن</th>
-    <th> البيان</th>
+                  <div class=" row py-3 "> 
 
-  </tr>
-</thead>
-<tbody>
+                        <div class="col-6" >
+                          <label> الحساب </label>
+                          <input type="text"  name ="current" placeholder="الصندوق الأساسي" disabled>
+                      </div>
 
-
-</tbody>
-</table>
-
-       <div class="row justify-content-end">
-            <div class="col-4">
-                <button type="submit" name="add"> إضافة </button>
-                <button type="submit" name="modify"> تعديل </button>
-                <button type="submit" name="delete"> حذف </button>
-                <button type="submit" name="print"> طباعة </button>
-                <button type="submit" name="close"> إغلاق </button>
+                      <div class="col-6">
+                          <label name=" "> رقم الإيصال </label>
+                          <input type=number  name =" " value= '' >
+                      
+                      </div>
+                  </div>
+              
+            <div class=" row "> 
+                  <div class="col-6" >
+                    <label name=" "> العملة </label>
+                    <input type="text" name =" " value= '' >
                 
+                  </div>
+                  <div class="col-6">
+                    <label name=" "> التاريخ</label>
+                    <input type="date" name =" " value='' >
+      
+                  </div>              
             </div>
-        </div>
-    </div>
+
+
+
+            <div class="row">
+                  <div class="col-6">
+                        
+                        <label for="note"> ملاحظات </label>
+                        <textarea rows="2" type="text" name="note"> </textarea>          
+                  </div>
+                        
+            </div>
+
+          <div class="row justify-content-center py-5">
+            <div class="col-10">
+              <table  class="table text-center table-bordered table-hover">
+                <thead class="bg-primary">
+                    <tr>
+                      <th> الحساب</th>
+                      <th> دائن</th>
+                      <th> البيان</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+            <div class="row justify-content-end py-5">
+              <div class="col-10">
+                  <button type="submit" class="btn btn-primary" name="add"> إضافة </button>
+                  <button type="submit" class="btn btn-primary" name="modify"> تعديل </button>
+                  <button type="submit" class="btn btn-primary" name="delete"> حذف </button>
+                  <button type="submit" class="btn btn-primary"  name="print"> طباعة </button>
+                  <button type="submit" class="btn btn-primary" name="close"> إغلاق </button>
+              </div>   
+              
+            </div>
+      </div>
   </form>  
 </body>
 </html>
