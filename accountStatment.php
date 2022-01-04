d0fafd69e4e17b2f0269913f0957ef861c7be18a
rgd <raghad.nanou@gmail.com>
2022-01-04 16:36:07 +0200

    account statment

--- accountStatment.php
+++ accountStatment.php
@@ -10,114 +10,129 @@ include('include/nav.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style></style>
+    <style>
+        body{
+            text-align: right;
+            background-color:LightGray;
+        }
+        .container{
+            background-color: #5F9EA0;
+            border-style:groove;
+               
+        }
+    </style>
 </head>
 
 <body>
     <form action="" method="post">
         <div class="container">
-            <div>
-                <label>الحساب</label>
-                <input class="account_auto" type="text" name="account" id="" 
-                value="<?php if(isset($_POST['account'])) echo $_POST['account'] ?>"
-                onclick="this.value=''">
-                
-                <label for="">العملة</label>
-                <select name="" id="">
-                    <option value="">ليرة سورية</option>
-                </select>
-                
-                <label for="">من تاريخ</label>
-                <input type="date" name="from_date" id="" min="" max="" 
-                        value="<?php if(isset($_POST['from_date'])) echo $_POST['from_date']; else echo date('Y-m-d') ?>">
-                <label for="">إلى تاريخ</label>
-                <input type="date" name="to_date" id="" min="" max="" 
-                        value="<?php if(isset($_POST['to_date'])) echo $_POST['to_date']; else echo date('Y-m-d') ?>">
-                <label for="">نوع التقرير</label>
-                <label for="">تفصيلي</label>
-                <input type="radio" name="rad_pay" id="delailes" onclick='clickradio()' checked>
-                <label for="">مختصر</label>  
-                <input type="radio" name="rad_pay" id="less" onclick='clickradio()' >
+            <div class="row py-2">
+                <div class="col-8 " >
+                    <div class="row py-2">
+                        <label for="" class=" col-md-1 form-label"> الحساب</label>
+                        <div class="col-md-4 has-validation">
+                            <input class="account_auto form-control" type="text" name="account" id="" value="<?php if(isset($_POST['account'])) echo $_POST['account'] ?>"
+                                    onclick="this.value=''" placeholder=" اسم الحساب" required >
+                            <div class="invalid-feedback">اسم الحساب الخاص بك مطلوب</div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <label for="" class=" col-md-1 form-label">العملة </label>
+                        <div class="col-md-4">
+                            <select name="currency" class="form-control">
+                                <option value="syrian-bounds">ليرة سورية</option>
+                            </select>
+                        </div>
+                    </div>
+            
+                </div>
+                <div class="col-4">
+                    <div class="row py-1">
+                        <label for="from_date">من تاريخ</label>
+                        <div class="col-md-7">
+                            <input type="date" name="from_date" class="form-control" min="" max="" 
+                                value="<?php if(isset($_POST['from_date'])) echo $_POST['from_date']; else echo date('Y-m-d') ?>">
+                        </div>
+                    </div>
+                    <div class="row ">
+                        <label for="to_date">إلى تاريخ</label>
+                        <div class="col-md-7">
+                            <input type="date" name="to_date" class="form-control" id="" min="" max="" 
+                                value="<?php if(isset($_POST['to_date'])) echo $_POST['to_date']; else echo date('Y-m-d') ?>">
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <?php
                 if (isset($_POST['view'])) {
                     echo "<h2 class='text-center'> كشف حساب : " . $_POST['account'] . "</h2>";
                     
                 }
                 ?>
-            <div class="row justify-content-center ">
-                <table hidden contenteditable='true' class="col-10 table table-bordered table-hover " name="table" id="tbl1">
-                    <thead class="text-center">
-                        <tr>
-                            <th contenteditable='false'>التاريخ</th>
-                            <th contenteditable='false'>رقم الحركة</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="row justify-content-center ">
+            <div class="row justify-content-center">
+                <div class="col-10">
                 
-                <table contenteditable='true' class="col-10 table table-bordered table-hover " name="table" id="tbl2">
-                    <thead class="text-center">
-                        <tr>
-                            <th contenteditable='false'>التاريخ</th>
-                            <th contenteditable='false'>مدين</th>
-                            <th contenteditable='false'>دائن</th>
-                            <th contenteditable='false'>الحساب المقابل</th>
-                            <th contenteditable='false'>البيان</th>
-                            <!-- <th contenteditable='false'>رقم الحركة</th>
-                            <th contenteditable='false'>اسم الحركة</th> -->
-                            <!-- <th contenteditable='false'>القيمة</th> -->
-                            <th contenteditable='false'>رصيد الحركة</th>
+                    <table contenteditable='true' class="table table-bordered table-hover " name="table" id="tbl2">
+                        <thead class="text-center">
+                            <tr>
+                                <th contenteditable='false'>التاريخ</th>
+                                <th contenteditable='false'>مدين</th>
+                                <th contenteditable='false'>دائن</th>
+                                <th contenteditable='false'>الحساب المقابل</th>
+                                <th contenteditable='false'>البيان</th>
+                                <!-- <th contenteditable='false'>رقم الحركة</th>
+                                <th contenteditable='false'>اسم الحركة</th> -->
+                                <!-- <th contenteditable='false'>القيمة</th> -->
+                                <th contenteditable='false'>رصيد الحركة</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
+                            </tr>
+                        </thead>
+                        <tbody class="text-center">
+                            <?php
 
-                        if (isset($_POST['view'])) {
-                            $main_account_code = get_code_from_input($_POST['account']);
-                            $select_main_accounta_id_using_code_query = "select id,code from accounts where code = '$main_account_code'";
-                            $select_main_accounta_id_using_code_exec = mysqli_query($con, $select_main_accounta_id_using_code_query);
-                            $main_account_id = 0;
-                            if(mysqli_num_rows($select_main_accounta_id_using_code_exec) > 0)
-                            $main_account_id = mysqli_fetch_row($select_main_accounta_id_using_code_exec)[0];
-                            
-                            $select_account_statements_query = select('account_statements').where('main_account_id' , $main_account_id)."
-                             and date between '" . $_POST['from_date'] ."' and '". $_POST['to_date'] ."'";
-                            $select_account_statements_exec = mysqli_query($con , $select_account_statements_query);
-                            $current_currency = 0;
-                            if(mysqli_num_rows($select_account_statements_exec) > 0)
-                            while($row = mysqli_fetch_array($select_account_statements_exec)){
-                                $current_currency +=  ($row['maden'] - $row['daen']);
-                                echo "<tr>";
-                                echo "<td>" . $row['date'] . "</td>";
-                                echo "<td>" . $row['maden'] . "</td>";
-                                echo "<td>" . $row['daen'] . "</td>";
-                                $select_other_account_name_query = select('accounts' , ['id' , 'name']).where('id' , $row['other_account_id']);
-                                $select_other_account_name_exec = mysqli_query($con , $select_other_account_name_query);
-                                // echo "<td>" . $row['code_number'] . "</td>";
-                                echo "<td>" . mysqli_fetch_row($select_other_account_name_exec)[1] . "</td>";
-                                echo "<td>" . $row['note'] . "</td>";
-                                echo "<td>" . "$current_currency". "</td>";
-                                echo "</tr>";
+                            if (isset($_POST['view'])) {
+                                $main_account_code = get_code_from_input($_POST['account']);
+                                $select_main_accounta_id_using_code_query = "select id,code from accounts where code = '$main_account_code'";
+                                $select_main_accounta_id_using_code_exec = mysqli_query($con, $select_main_accounta_id_using_code_query);
+                                $main_account_id = 0;
+                                if(mysqli_num_rows($select_main_accounta_id_using_code_exec) > 0)
+                                $main_account_id = mysqli_fetch_row($select_main_accounta_id_using_code_exec)[0];
+                                
+                                $select_account_statements_query = select('account_statements').where('main_account_id' , $main_account_id)."
+                                and date between '" . $_POST['from_date'] ."' and '". $_POST['to_date'] ."'";
+                                $select_account_statements_exec = mysqli_query($con , $select_account_statements_query);
+                                $current_currency = 0;
+                                if(mysqli_num_rows($select_account_statements_exec) > 0)
+                                while($row = mysqli_fetch_array($select_account_statements_exec)){
+                                    $current_currency +=  ($row['maden'] - $row['daen']);
+                                    echo "<tr>";
+                                    echo "<td>" . $row['date'] . "</td>";
+                                    echo "<td>" . $row['maden'] . "</td>";
+                                    echo "<td>" . $row['daen'] . "</td>";
+                                    $select_other_account_name_query = select('accounts' , ['id' , 'name']).where('id' , $row['other_account_id']);
+                                    $select_other_account_name_exec = mysqli_query($con , $select_other_account_name_query);
+                                    // echo "<td>" . $row['code_number'] . "</td>";
+                                    echo "<td>" . mysqli_fetch_row($select_other_account_name_exec)[1] . "</td>";
+                                    echo "<td>" . $row['note'] . "</td>";
+                                    echo "<td>" . "$current_currency". "</td>";
+                                    echo "</tr>";
+                                }
                             }
-                        }
 
-                        ?>
-                    </tbody>
-                </table>
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="row justify-content-center align-items-start">
-                <button type="submit" name="view">معاينة</button>
-                <button type="submit">إغلاق</button>
+            <div class="row justify-content-end py-2">
+                <div class="col-3">
+                    <button type="submit" class="btn btn-secondary" name="view">معاينة</button>
+                    <button type="submit" name="" class="btn btn-secondary">طباعة</button>
+                    <button type="submit"class="btn btn-secondary">إغلاق</button>
+                    
+                </div>
             </div>
 
 
--- test_rgd.php
+++ test_rgd.php
@@ -2,17 +2,18 @@
 include('include/nav.php');
 ?>
 
-<a href="category_card.php">الصنف</a>
-<a href="item_card.php">المادة</a>
-<a href="account_list.php">قائمة الحساب</a>
-<a href="account_card.php">بطاقة حساب</a>
+<a href="category_card.php">الصنف</a> <br>
+<a href="item_card.php">المادة</a> <br>
+<a href="account_list.php">قائمة الحساب</a> <br>
+<a href="account_card.php">بطاقة حساب</a> <br>
 <a href="item_list.php">قائمة المواد</a> <br>
-<a href="test_account_card.php"> test account card</a> <br><br>
-<a href="test_open_file_pop_up.php"> test opn pop up</a> <br><br>
-<a href="rgd_payment.php"> test  payment </a> <br><br>
-<a href="rgd_catch.php"> test  catch </a> <br><br>
-<a href="rgd_report_item.php">حركة  مادة</a> <br> <br>
-<a href="report_comission.php">حركة كمسيون</a> <br> <br>
+<a href="test_account_card.php"> test account card</a> <br>
+<a href="test_open_file_pop_up.php"> test opn pop up</a> <br>
+<a href="rgd_payment.php"> test  payment </a> <br>
+<a href="rgd_catch.php"> test  catch </a> <br>
+<a href="rgd_report_item.php">حركة  مادة</a> <br> 
+<a href="report_comission.php">حركة كمسيون</a> <br> 
+<a href="accountStatment.php">كشف حساب</a> <br>
 
 <a href="test_raghad_stole.php">Theif</a>
 <?php
