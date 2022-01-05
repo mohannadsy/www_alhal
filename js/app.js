c62680448caa177aab230d5d40effeb038d109f8
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-01-05 10:44:28 -0800

    update 11

--- js/app.js
+++ js/app.js
@@ -14,22 +14,24 @@ function disableEnterKey(evt) {
 document.onkeypress = disableEnterKey;
 
 
-function addRows(table, numberOfRows , names , ids){
+function addRows(table, numberOfRows, names, ids) {
     let tbl = document.getElementById(table);
     let tbody = document.createElement('tbody');
-    tbody.setAttribute('class' , 'text-center');
-    for(i = 0 ; i< numberOfRows ; i++){
+    tbody.setAttribute('class', 'text-center');
+    for (i = 0; i < numberOfRows; i++) {
         let row = document.createElement('tr');
-        for(j = 0 ; j < tbl.rows[0].cells.length ; j++){
+        for (j = 0; j < tbl.rows[0].cells.length; j++) {
             let col = document.createElement('td');
             let input = document.createElement('input');
-            input.setAttribute('name' , names[j]);
-            input.setAttribute('id' , ids[j]+'_'+i);
-            if(j == 0){
-                input.setAttribute('value' , i+1);
-                input.setAttribute('readonly' , 'readonly');
+            input.setAttribute('name', names[j]);
+            input.setAttribute('id', ids[j] + '_' + i);
+            input.setAttribute('style', 'color:blue; min-width:100%; min-height:100%; padding:2%; text-align:center; border-radius:5px;');
+            col.setAttribute('style', 'padding:4;');
+            if (j == 0) {
+                input.setAttribute('value', i + 1);
+                input.setAttribute('readonly', 'readonly');
             }
-            input.setAttribute('class' , 'text-center');
+            input.setAttribute('class', 'text-center');
             col.appendChild(input);
             row.appendChild(col);
         }
