4725e6cbfea431042e20671e77cb6b625185e56f
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-01-10 12:53:27 -0800

    update 16

--- include/nav.php
+++ include/nav.php
@@ -12,18 +12,42 @@
     include('helper/html_functions.php');
     include('helper/links.php');
 ?>
+
 <html dir="rtl" lang="ar">
 <ul id='nav' class="nav nav-tabs navbar-light bg-light">
   <li class="nav-item">
-    <a class="nav-link active" href="index.php">Home</a>
+    <a class="nav-link active" href="ready.php">Home</a>
   </li>
   <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">ملف</a>
+    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" 
+    aria-expanded="false" >ملف</a>
     <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">فتح ملف</a>
+      <a class="dropdown-item"  href="open_file.php" >فتح ملف</a>
       <a class="dropdown-item" href="#">ملف جديد</a>
       <a class="dropdown-item" href="#">استيراد</a>
   </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" 
+    aria-expanded="false" >حسابات</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item"  href="account_card.php" >إنشاء حساب</a>
+      <a class="dropdown-item" href="accountStatment.php">كشف حساب</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" 
+    aria-expanded="false" >مواد</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item"  href="category_card.php" >إنشاء صنف</a>
+      <a class="dropdown-item" href="item_card.php">إنشاء مادة</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" 
+    aria-expanded="false" >سندات</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item"  href="payment_bonds.php" >سند قبض</a>
+      <a class="dropdown-item" href="catch_bonds.php">سند دفع</a>
+  </li>
+
   <li class="nav-item">
     <a class="nav-link" href="test_index.php">Test</a>
   </li>
--- ready.php
+++ ready.php
@@ -2,7 +2,73 @@
 include('include/nav.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .button-63 {
+  align-items: center;
+  background-image:linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  display: flex;
+  font-family: Phantomsans, sans-serif;
+  font-size: 20px;
+  justify-content: center;
+  line-height: 1em;
+  max-width: 100%;
+  min-width: 140px;
+  padding: 19px 24px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+}
 
+.button-63:active,
+.button-63:hover {
+  outline: 0;
+}
+
+@media (min-width: 768px) {
+  .button-63 {
+    font-size: 24px;
+    min-width: 196px;
+  }
+}
+
+    </style>
+</head>
+<body>
+    <div>
+        <button class="button-63" role="button" onclick = "window.open('com_bill.php' , '_self')">فاتورة جديدة</button>
+    </div>
+    <div>
+        <button class="button-63" role="button" onclick = "window.open('account_card.php' , '_self')">حساب جديد</button>
+    </div>
+    <div>
+        <button class="button-63" role="button" onclick = "window.open('payment_bonds.php' , '_self')">سند دفع</button>
+    </div>
+    <div>
+        <button class="button-63" role="button" onclick = "window.open('catch_bonds.php' , '_self')">سند قبض</button>
+    </div>
+    <div>
+        <button class="button-63" role="button" onclick = "window.open('item_card.php' , '_self')">إضافة مواد</button>
+    </div>
+    
+
+
+</body>
+</html>
 
 <?php
 include('include/footer.php');
