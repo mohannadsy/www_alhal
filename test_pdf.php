533e575a2e723fa8ee93971f8423a06f4a4b3840
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-01-15 00:39:05 -0800

    tcpdf

--- test_pdf.php
+++ test_pdf.php
@@ -1,182 +1,100 @@
-<!-- <html>
+<?php
+//============================================================+
+// File name   : example_001.php
+// Begin       : 2008-03-04
+// Last Update : 2013-05-14
+//
+// Description : Example 001 for TCPDF class
+//               Default Header and Footer
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
 
-<head>
-    <style>
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-        
-        td {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-        
-        th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-            background-color: #111;
-            color: white;
-        }
-        
-        tr:nth-child(odd) {
-            background-color: #dddddd;
-        }
-    </style>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            var form = $('.form'),
-                cache_width = form.width(),
-                a4 = [595.28, 841.89]; // for a4 size paper width and height  
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Default Header and Footer
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
 
-            $('#create_pdf').on('click', function() {
-                $('body').scrollTop(0);
-                createPDF();
-            });
+// Include the main TCPDF library (search for installation path).
+require_once('tcPDF\tcpdf_include.php');
 
-            function createPDF() {
-                getCanvas().then(function(canvas) {
-                    var
-                        img = canvas.toDataURL("image/png"),
-                        doc = new jsPDF({
-                            unit: 'px',
-                            format: 'a4'
-                        });
-                    doc.addImage(img, 'JPEG', 20, 20);
-                    //doc.output('dataurlnewwindow');
-                    doc.save('filename.pdf') 
-                    form.width(cache_width);
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-//                   var string = doc.output('datauristring');
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
 
-                    // var iframe = "<iframe width='100%' height='100%' src='" + string + "'></iframe>"
 
-                    // var x = window.open();
-                    // x.document.open();
-                    // x.document.write(iframe);
-                    // x.document.close();
-                });
-            }
 
-            function getCanvas() {
-                form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
-                return html2canvas(form, {
-                    imageTimeout: 2000,
-                    removeContainer: true
-                });
-            }
-        });
-    </script>
-</head>
-<h1>How To Convert HTML To PDF using JavaScript - Websolutionstuff</h1>
-<form class="form">
-    <table>
-        <tbody>
-            <tr>
-                <th>Company Name</th>
-                <th>Employee Name</th>
-                <th>Country</th>
-            </tr>
-            <tr>
-                <td>Dell</td>
-                <td>Maria</td>
-                <td>Germany</td>
-            </tr>
-            <tr>
-                <td>Asus</td>
-                <td>Francisco</td>
-                <td>Mexico</td>
-            </tr>
-            <tr>
-                <td>Apple</td>
-                <td>Roland</td>
-                <td>Austria</td>
-            </tr>
-            <tr>
-                <td>HP</td>
-                <td>Helen</td>
-                <td>UK</td>
-            </tr>
-            <tr>
-                <td>Lenovo</td>
-                <td>Yoshi</td>
-                <td>Canada</td>
-            </tr>
-            <tr>
-                <td>Accer</td>
-                <td>Rovelli</td>
-                <td>Italy</td>
-            </tr>
-        </tbody>
-    </table><br>
-    <input type="button" id="create_pdf" value="Generate PDF">
-</form>
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH);
+$pdf->setFooterData(array(0,64,0), array(0,64,128));
 
-</html> -->
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div id="pdf1">
-        <p>Hi I am Sara</p>
-        <input type="text" name="" id="">
-        <p>What are you doing</p>
-        <input type="button" value="">
-    </div>
-    <button onclick="createPDF()">Create PDF</button>
-    <div id="example1"></div>
-</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfobject/2.2.7/pdfobject.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfobject/2.2.7/pdfobject.min.js"></script>
-<script>
-    //  var doc = new jsPDF();
-    //  function createPDF(){
-    //     // doc.fromHTML(`<html><head><title>${title}</title></head><body>` +
-    //     //  document.getElementById(divId).innerHTML + `</body></html>`);
-    //     // doc.text(20,20,'Invoice', { align: 'left' });
-    //     //doc.output('save','hello');
-    //     var tt = document.getElementById('pdf1').value;
-    //     var someElementToString = tt.toString();
-    //     //var someElement = document.getElementById("id");
-    //     //var someElementToString = someElement.toString();
-    //     //var someElementToString = tt.innerHTML;
-    //     doc.text(someElementToString,20,20);
-    //     window.open(doc.output('bloburl'));
-    // }
-    
-    
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-var doc = new jsPDF();          
-var elementHandler = {
-  '#ignorePDF': function (element, renderer) {
-    return true;
-  }
-};
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 
-var source1 = window.document.getElementById('pdf1');
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
-doc.fromHTML(
-    source1,
-    15,
-    15,
-    {
-      'width': 180,'elementHandlers': elementHandler
-    });
-    window.open(doc.output('bloburl'));
-</script>
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-   
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+	require_once(dirname(__FILE__).'/lang/eng.php');
+	$pdf->setLanguageArray($l);
+}
 
-</html>
+// ---------------------------------------------------------
+
+// set default font subsetting mode
+$pdf->setFontSubsetting(true);
+
+// Set font
+// dejavusans is a UTF-8 Unicode font, if you only need to
+// print standard ASCII chars, you can use core fonts like
+// helvetica or times to reduce file size.
+$pdf->SetFont('dejavusans', '', 14, '', true);
+
+// Add a page
+// This method has several options, check the source code documentation for more information.
+$pdf->AddPage();
+
+// set text shadow effect
+$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+
+// Set some content to print
+$html = <<<EOD
+<h1>Hello World !</h1>
+EOD;
+
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+$pdf->Output('helo_world.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//===========
