7bfba8f76a99ff4672aa29599d4ae6f9cda20b72
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-01-15 15:46:56 -0800

    tcPDF

--- test_pdf.php
+++ test_pdf.php
@@ -1,11 +1,51 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form action="" method="post">
+	<h1>This is my page</h1>
+	<a href="http://www.stackoverflow.com/">
+   		 <button>Click me</button>
+	</a>
+	<button type="submit"name='create_pdf' id="btn-grp">create pdf</button>
+	</form>
+</body>
+</html>
+<!-- <?php
+if(isset($_POST["create_pdf"])){
+	require_once('tcpdf/tcpdf.php');
+	$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+	//$pdf->setPrintHeader(false);
+	$pdf->AddPage();
+	$html = '<h1>Welcome to <a href="http://techbriefers.com/" style="text-decoration:none;padding: 10px;"> <span style="background-color:#ef3e47;color:#fff;"> Tech</span><span style="background-color:#fff1f0;color:#000;">Briefers</span> </a>!</h1>
+	<i>This is the two minute example of TCPDF library by <a href="http://techbriefers.com/">techbriefers</a>.</i>
+	<h2>What is Lorem Ipsum?</h2>
+	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>';
+	
+	$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+	if (ob_get_contents()) ob_end_clean();
+	$pdf->output('pdfReport.pdf', 'I');
+	//$pdf->output('pdfReport.pdf', 'I', '_blank');
+	//window.open('pdfReport.pdf', '_blank');
+	//window.open(doc.output('bloburl'));
+	//window.open(pdf('bloburl'));
+	}?> -->
+
+
 <?php
+if(isset($_POST["create_pdf"])){
 //============================================================+
-// File name   : example_001.php
+// File name   : example_003.php
 // Begin       : 2008-03-04
 // Last Update : 2013-05-14
 //
-// Description : Example 001 for TCPDF class
-//               Default Header and Footer
+// Description : Example 003 for TCPDF class
+//               Custom Header and Footer
 //
 // Author: Nicola Asuni
 //
@@ -19,25 +59,51 @@
 /**
  * Creates an example PDF TEST document using TCPDF
  * @package com.tecnick.tcpdf
- * @abstract TCPDF - Example: Default Header and Footer
+ * @abstract TCPDF - Example: Custom Header and Footer
  * @author Nicola Asuni
  * @since 2008-03-04
  */
 
 // Include the main TCPDF library (search for installation path).
-require_once('tcPDF\tcpdf_include.php');
+require_once('tcpdf/tcpdf.php');
+
+// Extend the TCPDF class to create custom Header and Footer
+class MYPDF extends TCPDF {
+
+    //Page header
+    public function Header() {
+        // Logo
+        $image_file = K_PATH_IMAGES.'logo_example.jpg';
+        $this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 20);
+        // Title
+        $this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+    }
+
+    // Page footer
+    public function Footer() {
+        // Position at 15 mm from bottom
+        $this->SetY(-15);
+        // Set font
+        $this->SetFont('helvetica', 'I', 8);
+        // Page number
+        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+    }
+}
 
 // create new PDF document
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-
-
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 003');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
 // set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH);
-$pdf->setFooterData(array(0,64,0), array(0,64,128));
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -59,42 +125,35 @@ $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 // set some language-dependent strings (optional)
 if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-	require_once(dirname(__FILE__).'/lang/eng.php');
-	$pdf->setLanguageArray($l);
+    require_once(dirname(__FILE__).'/lang/eng.php');
+    $pdf->setLanguageArray($l);
 }
 
 // ---------------------------------------------------------
 
-// set default font subsetting mode
-$pdf->setFontSubsetting(true);
+// set font
+$pdf->SetFont('times', 'BI', 12);
 
-// Set font
-// dejavusans is a UTF-8 Unicode font, if you only need to
-// print standard ASCII chars, you can use core fonts like
-// helvetica or times to reduce file size.
-$pdf->SetFont('dejavusans', '', 14, '', true);
-
-// Add a page
-// This method has several options, check the source code documentation for more information.
+// add a page
 $pdf->AddPage();
 
-// set text shadow effect
-$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+// set some text to print
+$txt = <<<EOD
+TCPDF Example 003
 
-// Set some content to print
-$html = <<<EOD
-<h1>Hello World !</h1>
+Custom page header and footer are defined by extending the TCPDF class and overriding the Header() and Footer() methods.
 EOD;
 
-// Print text using writeHTMLCell()
-$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+// print a block of text using Write()
+$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
 
 // ---------------------------------------------------------
 
-// Close and output PDF document
-// This method has several options, check the source code documentation for more information.
-$pdf->Output('helo_world.pdf', 'I');
+//Close and output PDF document
+$pdf->Output('example_003.pdf', 'I');
 
 //============================================================+
 // END OF FILE
-//===========
+//============================================================+
+
+}?>
